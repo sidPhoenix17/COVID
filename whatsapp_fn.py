3a88d5846ac10b53c208b4ecd0735888822e81a4
bhasin-uphar <37235597+bhasin-uphar@users.noreply.github.com>
2020-04-25 21:32:43 +0530

    basic code for whatsapp integration

--- whatsapp_fn.py
+++ whatsapp_fn.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+from flask import Flask
+from flask import jsonify
+from flask import request
 import requests
+import http.client
 import json
-from settings import server_type, whatsapp_api_url, whatsapp_api_password,auth_code
+from datetime import datetime
+import urllib3
+import pymysql
 
+#from settings import server_type, whatsapp_api_url, whatsapp_api_password,auth_code
 
-# In[ ]:
+whatsapp_api_url = "https://100.24.50.36:9090"
+whatsapp_api_password = "Khairnar@123"
 
-
-
-
-
-# In[ ]:
+app = Flask(__name__)
 
 
 #Where to get this from
@@ -25,15 +23,14 @@ def get_auth_key():
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Basic <base64(username:password)>',
-        'Authorization': auth_code
+        'Authorization': 'Basic YWRtaW46S2hhaXJuYXJAMTIz'
             }
     response = requests.request("POST", url, headers=headers, data = json.dumps(payload),verify=False)
     rs=response.text
     json_data=json.loads(rs)
     return json_data["users"][0]["token"]
 
-
-# In[ ]:
+authkey=get_auth_key()
 
 
 def send_whatsapp_message(url,to,message,preview_url=False):
@@ -66,21 +63,57 @@ def send_whatsapp_message_image(url,to,media_link,media_caption):
     return output
 
 
-# In[ ]:
+aws_host ="stg.covidsos.org"
+usr = "uphar"
+pas = "UpH4R@c0VId"
+db = "covidsos"
+
+@app.route('/', methods=['POST','GET'])
+def Get_Message():
+    now = str(datetime.now().date())
+    now_time = str(datetime.now())
+
+    print("_______________________________________________________________________")
+
+    print("entered")
+    cnt=0
+    response = request.json
+    print(response)
+    
+    cnx = pymysql.connect(user=usr, max_allowed_packet= 1073741824, password=pas, host=aws_host, database=db)
+    
+    
+    try:
+        frm = str(response["messages"][0]["from"])
+        text=str(response["messages"][0]["text"]["body"])
+        
+        checkState="SELECT name from covidsos.volunteers where mob_number='"+frm[2:]+"'"
+        cursor = cnx.cursor()
+        cursor.execute(checkState)
+        records = cursor.fetchone()
+        
+        if records == None:
+            send_whatsapp_message(whatsapp_api_url,frm,'not volunteer')
+        else:
+            name = list(records)[0]
+            send_whatsapp_message(whatsapp_api_url,frm,name)
+
+        print('\n'+frm+'\n')
 
 
+        
+    except Exception as e:
+        
+        print(e)
 
-# x =send_whatsapp_message(whatsapp_api_url,'919582148040','Testing message')
+    print("****************************************************************************")
 
+    return ""
 
-# In[ ]:
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host = '0.0.0.0', port=4000)
 
+        
 
-
-
-
-# In[ ]:
-
-
-
-
+        
