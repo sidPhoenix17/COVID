d29998929722f7f16e9c92b3f6e8c153bd48b09d
sidPhoenix17 <siddharthnibjiya@gmail.com>
2020-05-13 22:56:54 +0530

    commenting whatsapp functions & adding full address field in secure APIs

--- apis.py
+++ apis.py
@@ -43,7 +43,7 @@ from message_templates import old_reg_sms, new_reg_sms, new_request_sms, new_req
 
 from settings import server_type, SECRET_KEY, neighbourhood_radius, search_radius
 
-from whatsapp_fn import send_moderator_msg
+# from whatsapp_fn import send_moderator_msg
 
 import mailer_fn as mailer
 import cred_config as cc
@@ -125,8 +125,8 @@ def create_request():
         save_request_sms_url(uuid, 'verify_link', url_start + "verify/{uuid}".format(uuid=uuid))
         moderator_list = get_moderator_list()
         for i_number in moderator_list:
-            send_moderator_msg(i_number,mod_sms_text,preview_url=True)
-            # send_sms(mod_sms_text, sms_to=int(i_number), sms_type='transactional', send=True)
+            # send_moderator_msg(i_number,mod_sms_text,preview_url=True)
+            send_sms(mod_sms_text, sms_to=int(i_number), sms_type='transactional', send=True)
     return json.dumps(response)
 
 
@@ -228,8 +228,8 @@ def ngo_request_form(*args, **kwargs):
         save_request_sms_url(uuid, 'verify_link', url_start + "verify/{uuid}".format(uuid=uuid))
         moderator_list = get_moderator_list()
         for i_number in moderator_list:
-            send_moderator_msg(i_number,mod_sms_text,preview_url=True)
-            # send_sms(mod_sms_text, sms_to=int(i_number), sms_type='transactional', send=True)
+            # send_moderator_msg(i_number,mod_sms_text,preview_url=True)
+            send_sms(mod_sms_text, sms_to=int(i_number), sms_type='transactional', send=True)
 
     response = {'Response': {}, 'status': x1, 'string_response': y1}
     return response
@@ -513,8 +513,8 @@ def assign_request_to_volunteer(volunteer_id, request_id, matched_by, org):
                                                        r_mob_number=r_df.loc[0, 'mob_number'])
             moderator_list = get_moderator_list()
             for i_number in moderator_list:
-                send_moderator_msg(i_number, m_sms_text, preview_url=True)
-#                send_sms(m_sms_text, int(i_number), sms_type='transactional', send=True)
+                # send_moderator_msg(i_number, m_sms_text, preview_url=True)
+               send_sms(m_sms_text, int(i_number), sms_type='transactional', send=True)
         else:
             return {'status': False, 'string_response': 'Request already assigned/closed/completed', 'Response': {}}
     return response
@@ -752,12 +752,13 @@ def admin_completed_requests(*args, **kwargs):
         return json.dumps({'Response': response.to_dict('records'), 'status': True, 'string_response': 'Request data extracted'},
                           default=datetime_converter)
 
-@app.route('/completed_requests', methods=['GET'])
-@capture_api_exception
-def completed_requests(*args, **kwargs):
-    response = get_completed_requests()
-    return json.dumps({'Response': response.to_dict('records'), 'status': True, 'string_response': 'Request data extracted'},
-                      default=datetime_converter)
+# Removing this as not being used anywhere
+# @app.route('/completed_requests', methods=['GET'])
+# @capture_api_exception
+# def completed_requests(*args, **kwargs):
+#     response = get_completed_requests()
+#     return json.dumps({'Response': response.to_dict('records'), 'status': True, 'string_response': 'Request data extracted'},
+#                       default=datetime_converter)
 
 
 @app.route('/admin_pending_requests', methods=['GET'])
@@ -872,12 +873,8 @@ def task_completed(*args, **kwargs):
                                         status=status, v_name=v_df.loc[0, 'name'],
                                         v_mob_number=v_df.loc[0, 'mob_number'],
                                         status_message=status_message)
-            send_moderator_msg(i_number,mod_sms_text,preview_url=True)
-            # send_sms(request_closed_m_sms.format(r_id=r_df.loc[0, 'r_id'], r_name=r_df.loc[0, 'name'],
-            #                                      r_mob_number=r_df.loc[0, 'mob_number'],
-            #                                      status=status, v_name=v_df.loc[0, 'name'],
-            #                                      v_mob_number=v_df.loc[0, 'mob_number'],
-            #                                      status_message=status_message), i_number)
+            # send_moderator_msg(i_number,mod_sms_text,preview_url=True)
+            send_sms(mod_sms_text, int(i_number))
         send_sms(request_closed_r_sms.format(status=status), int(r_df.loc[0, 'mob_number']))
     if (status == 'cancelled'):
         message_all_volunteers(request_uuid, neighbourhood_radius, search_radius)
@@ -915,13 +912,8 @@ def admin_task_completed(*args, **kwargs):
                                                    v_mob_number=v_df.loc[0, 'mob_number'], status=status,
                                                    user_name=user_df.loc[0, 'name'],
                                                    status_message=status_message)
-            send_moderator_msg(i_number,mod_sms_text,preview_url=True)
-            # send_sms(a_request_closed_m_sms.format(r_id=r_df.loc[0, 'r_id'], r_name=r_df.loc[0, 'name'],
-            #                                        r_mob_number=r_df.loc[0, 'mob_number'],
-            #                                        v_name=v_df.loc[0, 'name'],
-            #                                        v_mob_number=v_df.loc[0, 'mob_number'], status=status,
-            #                                        user_name=user_df.loc[0, 'name'],
-            #                                        status_message=status_message), i_number)
+            # send_moderator_msg(i_number,mod_sms_text,preview_url=True)
+            send_sms(mod_sms_text, int(i_number))
         send_sms(a_request_closed_r_sms.format(status=status), int(r_df.loc[0, 'mob_number']))
     if (status == 'cancelled'):
         message_all_volunteers(request_uuid, neighbourhood_radius, search_radius)
